f540a4bfe7827be63111817f278da8ae19c3d121
hasankhan-123 <hasankha1023@gmail.com>
2023-05-03 23:58:37 +0600

    Cors added

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')()
+const cors = require('cors')
 const app = express;
 const port = 5000;
 
+// use cors
+app.use(cors())
 
+const allChefs = require('./Data/BestchefsData.json')
+
+// get Api data
+app.get('/all-chefs', (req, res) => {
+    res.send(allChefs)
+})
+
+
+// app get
 app.get('/', (req, res) => {
     res.send('Hello world Api is coming soon...')
 })
 
+// app listen
 app.listen(port, () => {
     console.log('This server on:', port)
 })
