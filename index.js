7e7ee39fde4116662873a8570a97e47350f7e762
hasankhan-123 <hasankha1023@gmail.com>
2023-05-04 21:06:08 +0600

    some problem fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')()
+const express = require('express')
 const cors = require('cors')
-const app = express;
-const port = 5000;
+const app = express();
+const port =  process.env.PORT || 5000;
 
 // use cors
 app.use(cors())
